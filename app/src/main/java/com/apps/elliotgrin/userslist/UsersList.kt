b9c0d7c86d044a4df1f9e7847fb1da02a16be141
Roman Kuznetsov <rpkuznetsov@gmail.com>
2019-01-08 23:04:28 +0300

    Add base structure

---
+++ app/src/main/java/com/apps/elliotgrin/userslist/UsersList.kt
@@ -0,0 +1,12 @@
+package com.apps.elliotgrin.userslist
+
+import android.app.Application
+
+class UsersList : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+}
---
+++ app/src/main/java/com/apps/elliotgrin/userslist/ui/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.apps.elliotgrin.userslist.ui.main
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.apps.elliotgrin.userslist.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
