573157c1d7466698c1fabedcd1acda92fc539863
Roman Kuznetsov <rpkuznetsov@gmail.com>
2019-01-08 23:42:46 +0300

    Add dependencies

--- app/src/main/java/com/apps/elliotgrin/userslist/ui/main/MainActivity.kt
+++ app/src/main/java/com/apps/elliotgrin/userslist/ui/main/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.apps.elliotgrin.userslist.ui.main
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.apps.elliotgrin.userslist.R
 
