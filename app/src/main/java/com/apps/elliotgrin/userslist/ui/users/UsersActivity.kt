de630255eb9c453653b7b4150f82d43a2ddf5ac2
Roman Kuznetsov <rpkuznetsov@gmail.com>
2019-01-13 17:46:34 +0300

    Remove useless UsersFragment

--- app/src/main/java/com/apps/elliotgrin/userslist/ui/users/UsersActivity.kt
+++ app/src/main/java/com/apps/elliotgrin/userslist/ui/users/UsersActivity.kt
@@ -4,15 +4,15 @@ import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.apps.elliotgrin.userslist.R
 import com.apps.elliotgrin.userslist.data.model.User
-import com.apps.elliotgrin.userslist.ui.base.BaseFragment
+import com.apps.elliotgrin.userslist.ui.base.BaseActivity
 import com.apps.elliotgrin.userslist.ui.create.CreateUserActivity
 import com.apps.elliotgrin.userslist.util.ext.setVisible
-import kotlinx.android.synthetic.main.fragment_users.*
+import kotlinx.android.synthetic.main.activity_users.*
 
-class UsersFragment : BaseFragment<UsersState, UsersViewModel>(UsersViewModel::class) {
+class UsersActivity : BaseActivity<UsersState, UsersViewModel>(UsersViewModel::class) {
 
     override val layoutId: Int
-        get() = R.layout.fragment_users
+        get() = R.layout.activity_users
 
     override fun whenState(state: UsersState) = when (state) {
         is UsersState.StateLoading -> showLoading(state.isLoading)
@@ -21,7 +21,7 @@ class UsersFragment : BaseFragment<UsersState, UsersViewModel>(UsersViewModel::c
     }
 
     override fun initViews() {
-        val layoutManager = LinearLayoutManager(context)
+        val layoutManager = LinearLayoutManager(this)
         recyclerView.layoutManager = layoutManager
 
         fab.setOnClickListener { startCreateUserActivity(null) }
@@ -33,21 +33,16 @@ class UsersFragment : BaseFragment<UsersState, UsersViewModel>(UsersViewModel::c
     }
 
     private fun showUsers(users: List<User>) {
-        context?.let { context ->
-            val adapter = UsersRecyclerAdapter(users, context) { user -> startCreateUserActivity(user) }
-            recyclerView.adapter = adapter
-        }
+        val adapter = UsersRecyclerAdapter(users, this) { user -> startCreateUserActivity(user) }
+        recyclerView.adapter = adapter
     }
 
     private fun showError(error: String) {
-        Toast.makeText(context, error, Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, error, Toast.LENGTH_SHORT).show()
     }
 
     private fun startCreateUserActivity(user: User?) {
-        context?.let { context ->
-            val intent = CreateUserActivity.newIntent(context, user)
-            startActivity(intent)
-        }
+        val intent = CreateUserActivity.newIntent(this, user)
+        startActivity(intent)
     }
-
 }
